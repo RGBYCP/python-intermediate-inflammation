f0e202db56940c0a073c1fdd375c496a39fc5ac6
RGBYCP <bestenborstel@gmail.com>
2022-05-24 15:44:43 +0200

    Add initial test cases for daily_max() and daily_min()

--- tests/test_models.py
+++ tests/test_models.py
@@ -2,6 +2,16 @@
 
 import numpy as np
 import numpy.testing as npt
+import pytest
+
+
+def test_daily_min_string():
+    """Test for TypeError when passing strings"""
+    from inflammation.models import daily_min
+
+    with pytest.raises(TypeError):
+        error_expected = daily_min([['Hello', 'there'], ['General', 'Kenobi']])
+
 
 
 def test_daily_mean_zeros():
@@ -29,3 +39,29 @@ def test_daily_mean_integers():
     # Need to use Numpy testing functions to compare arrays
     npt.assert_array_equal(daily_mean(test_input), test_result)
 
+
+
+def test_daily_max():
+    """" Test that max function works on a 2D array and returns 1D array
+    """
+    from inflammation.models import daily_max
+
+    test_input = np.array([[1, 2],
+                           [5, 7],
+                           [3, 4]])
+    test_result = np.array([5, 7])
+
+    npt.assert_array_equal(daily_max(test_input), test_result)
+
+
+def test_daily_min():
+    """" Test that min function works on a 2D array and returns 1D array
+    """
+    from inflammation.models import daily_min
+
+    test_input = np.array([[1, 2],
+                           [5, 7],
+                           [3, 4]])
+    test_result = np.array([1, 2])
+
+    npt.assert_array_equal(daily_min(test_input), test_result)
